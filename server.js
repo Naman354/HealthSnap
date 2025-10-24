3502a1f8077f9c10a162fba5268fce4f5c36196f
Naman354 <srinaman05@gmail.com>
2025-10-24 23:34:16 +0530

    Added server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app=express();
+
+app.use(express.json());
+
+app.use(".api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    res.send("Server is Live"); 
+});
+
+const PORT = process.env.PORT || 5500;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
