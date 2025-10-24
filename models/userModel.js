1542f176cd83e9dd6406a5508e14348f7854ca5b
Naman354 <srinaman05@gmail.com>
2025-10-24 22:53:22 +0530

    initial commit

---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+},
+{timestamps:true});
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+export const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@$%&*?])[A-Za-z\d!@$%&*?]{8,1}$/;
+
+export function validatePassword(password) {
+    return passwordRegex.test(password);
+}
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
